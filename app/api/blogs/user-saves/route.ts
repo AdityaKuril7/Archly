83993ab2aa31f4c9de295e817ca95edc803da7e5
AdityaKuril7 <adityakuril760@gmail.com>
2026-06-21 08:11:40 +0530

    refactor: enhance `user-saves` route with `verifyToken` and improved error handling
    - Replaced `userId` with `id` retrieved from `verifyToken` for better authentication.
    - Added validation for unauthorized access, returning status 401 if `id` is missing.

--- app/api/blogs/user-saves/route.ts
+++ app/api/blogs/user-saves/route.ts
@@ -1,14 +1,15 @@
 import {ErrorHandler} from "@/lib/errorHandler";
 import User from "@/models/user.model";
 import {NextRequest, NextResponse} from "next/server";
+import {verifyToken} from "@/lib/verifyauth";
 
 export async function POST(req: NextRequest) {
   try {
-    const { userId } = await req.json();
 
-    if (!userId) return ErrorHandler("userId required", 400);
+    const {id} = await verifyToken(req)
+    if(!id) return ErrorHandler("Unauthorized",401)
 
-    const blogs = await User.findById(userId)
+    const blogs = await User.findById(id)
       .select("savedBlogs")
       .populate("savedBlogs");
 
