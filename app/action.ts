0427f608418f5067cf9331db639cc0a4395b9a51
AdityaKuril7 <adityakuril760@gmail.com>
2026-06-25 20:59:06 +0530

    refactor: remove unnecessary console log from `deleteToken` function

--- app/action.ts
+++ app/action.ts
@@ -4,6 +4,5 @@ import { cookies } from 'next/headers'
 
 export async function deleteToken() {
   const cookieStore = await cookies()
-  console.log("Performing delete action")
   cookieStore.delete('token')
 }
