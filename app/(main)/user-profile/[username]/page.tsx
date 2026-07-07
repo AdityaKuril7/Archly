5096461dcb320ab9901818b5fc0fe97b000009db
AdityaKuril7 <adityakuril760@gmail.com>
2026-07-07 22:37:08 +0530

    fix: remove unused isConnectionVisible and toggleConnection from Profile component

--- app/(main)/user-profile/[username]/page.tsx
+++ app/(main)/user-profile/[username]/page.tsx
@@ -28,8 +28,6 @@ export default function Profile({
     fetchProfile,
     isEditCardVisible,
     toggleEditCard,
-    isConnectionVisible,
-    toggleConnection,
   } = useProfileStore();
 
   const userId = getUserId();
