0535a7060b15dda659c5dc6ceed601f0e646ec35
AdityaKuril7 <adityakuril760@gmail.com>
2026-08-16 10:51:03 +0530

    fix: update layout to conditionally adjust grid columns based on sidebar state

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -13,7 +13,9 @@ const layout = ({ children }: { children: React.ReactNode }) => {
     <div className="h-screen w-full flex flex-col overflow-hidden">
       <Navbar />
 
-      <div className="grid grid-cols-3 w-full h-full">
+      <div
+        className={`grid ${isSidebarOpen ? "grid-cols-2" : "grid-cols-3"} w-full h-full`}
+      >
         <div className="justify-self-start">
           <AnimatePresence>{isSidebarOpen && <Sidebar />}</AnimatePresence>
         </div>
